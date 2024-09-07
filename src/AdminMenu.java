8c545f6fb66f26d9367b1a2e0743efe7e6c21779
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2024-09-07 23:15:54 +0700

    Complete MainMenu

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
 
 public class AdminMenu {
-    private static int choiceAdmin = -1;
+	private static int choiceAdmin = -1;
 
-    public static void adminMenu() {
-        while (choiceAdmin != 0) {
-            displayAdminMenu();
-            Scanner input = new Scanner(System.in);
-            choiceAdmin = input.nextInt();
-            switch (choiceAdmin) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    MainMenu.mainMenu();
-                    break;
-            }
-        }
-    }
+	public static void adminMenu() {
+		while (choiceAdmin != 0) {
+			displayAdminMenu();
+			Scanner input = new Scanner(System.in);
+			choiceAdmin = input.nextInt();
+			switch (choiceAdmin) {
+				case 1:
+					// See all customers
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					MainMenu.mainMenu();
+					break;
+			}
+		}
+	}
 
-    private static void displayAdminMenu() {
-        System.out.println("--------------------------------------------------");
-        System.out.println("1. See all Customers");
-        System.out.println("2. See all Rooms");
-        System.out.println("3. Add a Room");
-        System.out.println("4. Back to Main Menu");
-        System.out.println("--------------------------------------------------");
-        System.out.println("Please select a number for the menu option");
-    }
+	private static void displayAdminMenu() {
+		System.out.println("--------------------------------------------------");
+		System.out.println("1. See all Customers");
+		System.out.println("2. See all Rooms");
+		System.out.println("3. Add a Room");
+		System.out.println("4. Back to Main Menu");
+		System.out.println("--------------------------------------------------");
+		System.out.println("Please select a number for the menu option");
+	}
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,69 +6,75 @@ import java.util.Date;
 
 
 public class MainMenu {
-    private static final HotelResource hotelResource = HotelResource.getHotelResourceInstance();
-    private static final DataInput INPUT = DataInput.getDataInput();
-    private static int choice = -1;
+	private static final HotelResource hotelResource = HotelResource.getHotelResourceInstance();
+	private static final DataInput INPUT = DataInput.getDataInput();
+	private static int choice = -1;
 
-    public static void mainMenu() {
-        while (choice != 0) {
-            displayMainMenu();
-            choice = INPUT.getIntInput();
-            switch (choice) {
-                case 1:
-                    // Find rooms
-                    Date checkIn = null;
-                    Date checkOut = null;
-                    try {
-                        System.out.println("Enter CheckIn Date dd/MM/yyyy. Example: 02/01/2024");
-                        checkIn = INPUT.getDateInput();
-                        System.out.println("Enter CheckOut Date dd/MM/yyyy. Example: 02/01/2024");
-                        checkOut = INPUT.getDateInput();
-                    } catch (ParseException e) {
-                        System.out.println("Wrong format date");
-                        e.printStackTrace();
-                    }
-                    hotelResource.findARoom(checkIn, checkOut);
-                    // Reserve a room
-                    System.out.println("Would you like to book a room? (Please type y/n)");
-                    String text = INPUT.getStringInput().toLowerCase();
-                    while (text.equals("y") || text.equals("n")) {
-                        System.out.println("good");
-                    }
-                    String email = INPUT.getStringInput();
-                    System.out.println("Choose room number:");
-                    hotelResource.bookARoom(email, hotelResource.getRoom(INPUT.getStringInput()), checkIn, checkOut);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    System.out.println("Enter email formatted as something@domain.com");
-                    String emailCus = INPUT.getStringInput();
-                    System.out.println("What is your first name?");
-                    String firstName = INPUT.getStringInput();
-                    System.out.println("What is your last name?");
-                    String lastName = INPUT.getStringInput();
-                    hotelResource.createCustomer(emailCus, firstName, lastName);
-                    break;
-                case 4:
-                    AdminMenu.adminMenu();
-                    break;
-                case 5:
-                    System.exit(0);
-                    break;
-            }
-        }
-    }
+	public static void mainMenu() {
+		while (choice != 0) {
+			displayMainMenu();
+			choice = INPUT.getIntInput();
+			switch (choice) {
+				case 1:
+					// Find rooms
+					Date checkIn = null;
+					Date checkOut = null;
+					try {
+						System.out.println("Enter CheckIn Date dd/MM/yyyy. Example: 02/01/2024");
+						checkIn = INPUT.getDateInput();
+						System.out.println("Enter CheckOut Date dd/MM/yyyy. Example: 02/01/2024");
+						checkOut = INPUT.getDateInput();
+					} catch (ParseException e) {
+						System.out.println("Wrong format date");
+						e.printStackTrace();
+					}
+					hotelResource.findARoom(checkIn, checkOut);
+					// Reserve a room
+					System.out.println("Would you like to book a room? (Please type y/n)");
+					String text = INPUT.getStringInput();
+					while (!text.equalsIgnoreCase("y") || !text.equalsIgnoreCase("n")) {
+						System.out.println("Would you like to book a room? (Please type y/n)");
+						text = INPUT.getStringInput();
+					}
+					if (text.equalsIgnoreCase("y")) {
+						String email = INPUT.getStringInput();
+						System.out.println("Choose room number:");
+						hotelResource.bookARoom(email, hotelResource.getRoom(INPUT.getStringInput()), checkIn, checkOut);
+					}
+					break;
+				case 2:
+					// See my reservations
+					String eCus = INPUT.getStringInput();
+					hotelResource.getCustomerReservations(eCus);
+					break;
+				case 3:
+					System.out.println("Enter email formatted as something@domain.com");
+					String emailCus = INPUT.getStringInput();
+					System.out.println("What is your first name?");
+					String firstName = INPUT.getStringInput();
+					System.out.println("What is your last name?");
+					String lastName = INPUT.getStringInput();
+					hotelResource.createCustomer(emailCus, firstName, lastName);
+					break;
+				case 4:
+					AdminMenu.adminMenu();
+					break;
+				case 5:
+					System.exit(0);
+					break;
+			}
+		}
+	}
 
-    private static void displayMainMenu() {
-        System.out.println("Welcome to the Hotel Reservation Application");
-        System.out.println("--------------------------------------------------");
-        System.out.println("1. Find and reserve a room");
-        System.out.println("2. See my reservations");
-        System.out.println("3. Create an account");
-        System.out.println("4. Admin");
-        System.out.println("5. Exit");
-        System.out.println("--------------------------------------------------");
-        System.out.println("Please select a number for the menu option");
-    }
+	private static void displayMainMenu() {
+		System.out.println("Welcome to the Hotel Reservation Application");
+		System.out.println("--------------------------------------------------");
+		System.out.println("1. Find and reserve a room");
+		System.out.println("2. See my reservations");
+		System.out.println("3. Create an account");
+		System.out.println("4. Admin");
+		System.out.println("5. Exit");
+		System.out.println("--------------------------------------------------");
+		System.out.println("Please select a number for the menu option");
+	}
 }
